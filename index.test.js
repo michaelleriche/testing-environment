85a39ab0fd4fe83564c3bcbac5351482802a99da
michaelleriche <mikeleriche@gmail.com>
2020-05-20 14:22:14 -0400

    done for now

---
+++ index.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const { forEach, map } = require('./index');
+
+// const test = (desc, fn) => {
+//     console.log('---', desc);
+//     try {
+//         fn();
+//     } catch (err) {
+//         console.log(err.message);
+//     }
+// };
+
+
+
+it('the forEach function', () => {
+    let sum = 0;
+    forEach([1,2,3], (value) => {
+        sum = sum + value;
+    });
+
+    assert.strictEqual(sum, 6, 'expected forEach to sum the array');
+});
+
+it('the map function', () => {
+    const result = map([1,2,3], value => {
+        return value * 2;
+    });
+
+    assert.deepStrictEqual(result, [2,4,6]);
+});
